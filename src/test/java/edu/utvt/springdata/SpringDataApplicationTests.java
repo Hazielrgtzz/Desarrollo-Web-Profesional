8723ae5dcabbf1df663fce18babd1e2cba095dbd
Al3x2023 <al222210545@gmail.com>
2025-02-08 11:11:32 -0600

    first commit

---
+++ src/test/java/edu/utvt/springdata/SpringDataApplicationTests.java
@@ -0,0 +1,22 @@
+package edu.utvt.springdata;
+
+import edu.utvt.springdata.data.entities.Student;
+import edu.utvt.springdata.data.entities.repositories.StudentRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataApplicationTests {
+    @Autowired
+    private StudentRepository studentRepository;
+
+
+    @Test
+    void contextLoads() {
+
+        Student student = new Student("pedro", "picapiedra");
+        studentRepository.save(student);
+               }
+
+}
