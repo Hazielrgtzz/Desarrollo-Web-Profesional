a3778773934350a20e6cffee785fa328f7306958
hazielrgtzz <al222210641@gmail.com>
2025-03-08 09:39:05 -0600

    SpringData-Conexion

---
+++ src/main/java/edu/utvt/springdata/data/entities/repositories/PlayerRepository.java
@@ -0,0 +1,8 @@
+package edu.utvt.springdata.data.entities.repositories;
+
+import edu.utvt.springdata.data.entities.Player;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface PlayerRepository extends JpaRepository<Player,Long>  {
+}
